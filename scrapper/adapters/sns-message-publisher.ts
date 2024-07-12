76e59933b2e0f80ffbbb6130e046002b75cb8da7
Marcos Parreiras <marcosalp1998@gmail.com>
2024-07-12 11:58:13 -0300

    fix: a bug with aws credentials on SNS SDK

--- scrapper/adapters/sns-message-publisher.ts
+++ scrapper/adapters/sns-message-publisher.ts
@@ -7,11 +7,7 @@ export class SNSMessagePublisher implements MessagePublisher {
     private sns: SNS;
 
     public constructor() {
-        this.sns = new SNS({
-            accessKeyId: env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
-            region: env.AWS_REGION,
-        });
+        this.sns = new SNS();
     }
 
     public async send(channel: string, message: string): Promise<void> {
